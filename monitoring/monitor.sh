46b5bb5acbf5f69ab5571e1acc43baef4870f8c0
DonAurelio <aurelio.vivas@correounivalle.edu.co>
2018-10-29 10:59:07 -0500

    Testing monitoring

--- monitoring/monitor.sh
+++ monitoring/monitor.sh
@@ -4,15 +4,9 @@
 # https://nvidia.custhelp.com/app/answers/detail/a_id/3751/~/useful-nvidia-smi-queries
 # http://www.systeen.com/2016/05/07/bash-script-monitor-cpu-memory-disk-usage-linux/
 
+# SECONDS -- The number of seconds since the shell started or, if the parameter has been assigned an integer value, 
+# the number of seconds since the assignment plus the value that was assigned.
 
-
-GPU_METRICS=$(nvidia-smi --query-gpu=timestamp,name,driver_version,pstate,temperature.gpu,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used --format=csv
-)
-
-# CPU METRICTS
-
-
-printf "Memory\t\tDisk\t\tCPU\n"
 end=$((SECONDS+3600))
 while [ $SECONDS -lt $end ]; do
 
@@ -30,28 +24,31 @@ CPU_METRICS="$CPU_USAGE$CPU_TEMP$CPU_MEMORY_USAGE$CPU_MEMEORY_TOTAL$CPU_MEMEORY_
 
 # GPU METRICS
 # -timesptamp: The timestamp of where the query was made in format "YYYY/MM/DD HH:MM:SS.msec".
-GPU_TIMESTAMP=$(nvidia-smi --query-gpu=timestamp --format=csv,noheader,nounits)
+GPU_TIMESTAMP=$(nvidia-smi --query-gpu=timestamp --format=csv,noheader,nounits | awk 'NR=1{printf "%s\t\t",$1}')
 # -name: The official product name of the GPU. This is an alphanumeric string. For all products.
-GPU_NAME=$(nvidia-smi --query-gpu=name --format=csv,noheader,nounits)
+GPU_NAME=$(nvidia-smi --query-gpu=name --format=csv,noheader,nounits | awk 'NR=1{printf "%s\t\t",$1}')
 # -driver_version: The version of the installed NVIDIA display driver. This is an alphanumeric string.
-GPU_DRIVER=$(nvidia-smi --query-gpu=driver_version --format=csv,noheader,nounits)
+GPU_DRIVER=$(nvidia-smi --query-gpu=driver_version --format=csv,noheader,nounits | awk 'NR=1{printf "%s\t\t",$1}')
 # -pstate: The current performance state for the GPU. States range from P0 (maximum performance) 
 # 	to P12 (minimum performance).
-GPU_PST=$(nvidia-smi --query-gpu=pstate --format=csv,noheader,nounits)
+GPU_PST=$(nvidia-smi --query-gpu=pstate --format=csv,noheader,nounits | awk 'NR=1{printf "%s\t\t",$1}')
 # -temperature.gpu: Core GPU temperature. in degrees C.
-GPU_TEMP=$(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader,nounits)
+GPU_TEMP=$(nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader,nounits | awk 'NR=1{printf "%s\t\t",$1}')
 # -utilization.gpu: Percent of time over the past sample period during which one or more kernels was
-GPU_USAGE=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits)
 # 	executing on the GPU. The sample period may be between 1 second and 1/6 second depending on the product.
+GPU_USAGE=$(nvidia-smi --query-gpu=utilization.gpu --format=csv,noheader,nounits | awk 'NR=1{printf "%s\t\t",$1}')
 # -utilization.memory: Percent of time over the past sample period during which global (device) memory
 # 	was being read or written. The sample period may be between 1 second and 1/6 second depending on the product.
+GPU_MEMORY_USAGE=$(nvidia-smi --query-gpu=utilization.memory --format=csv,noheader,nounits | awk 'NR=1{printf "%s\t\t",$1}')
 # -memory.total: Total installed GPU memory.
-# -memory.free: Total free memory.
+GPU_MEMORY_TOTAL=$(nvidia-smi --query-gpu=memory.total --format=csv,noheader,nounits | awk 'NR=1{printf "%s\t\t",$1}')
 # -memory.used: Total memory allocated by active contexts.
+GPU_MEMORY_USED=$(nvidia-smi --query-gpu=memory.used --format=csv,noheader,nounits | awk 'NR=1{printf "%s\t\t",$1}')
+# -memory.free: Total free memory.
+GPU_MEMORY_FREE=$(nvidia-smi --query-gpu=memory.free --format=csv,noheader,nounits | awk 'NR=1{printf "%s\t\t",$1}')
 
-GPU_METRICS=$(nvidia-smi --query-gpu=timestamp,name,driver_version,pstate,temperature.gpu,utilization.gpu,utilization.memory,memory.total,memory.free,memory.used --format=csv
-)
-
-echo "$MEMORY$DISK$CPU"
+GPU_METRICS="$GPU_NAME$GPU_DRIVER$GPU_PST$GPU_TEMP$GPU_USAGE$GPU_MEMORY_USAGE$GPU_MEMORY_TOTAL$GPU_MEMORY_USAGE$GPU_MEMORY_FREE"
+echo $SECONDS
+echo "$CPU_METRICS$GPU_METRICS"
 sleep 5
 done
